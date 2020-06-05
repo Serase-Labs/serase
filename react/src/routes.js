88b3ac1459ea540313805e90360836cbb2b2b189
eumsmo <juanriven10@gmail.com>
2020-06-04 21:33:34 -0300

    📁 Arquivos do React-Native
    src: arquivos do projeto que não são os principais
    src/assets: imagens usadas no projeto
    src/pages: paginas do projeto
    src/components: componentes usados em mais de uma página do projeto
    
    routes: onde fica as rotas

---
+++ react/src/routes.js
@@ -0,0 +1,10 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+
+
+const Routes = createAppContainer(
+    createSwitchNavigator({
+
+    })
+);
+
+export default Routes;
