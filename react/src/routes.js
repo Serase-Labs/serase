4761fab2e7dad95d201924042421621b8fb42a12
Isabella Campos <isabellaemiliano14@gmail.com>
2020-06-10 15:28:47 -0300

    Página de Cadastro

--- react/src/routes.js
+++ react/src/routes.js
@@ -1,11 +1,13 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import Login from './pages/Login';
+import Cadastro from './pages/Cadastro'; 
 import VisualizacaoGeral from './pages/VisualizacaoGeral'; 
 
 const Routes = createAppContainer(
     createSwitchNavigator({
         Login,
+        Cadastro,
         VisualizacaoGeral,
     })
 );
