6eebe171e11b47c2fd4b6323e8ed7e0e652c9f77
Nicoly <nicolysantos51@gmail.com>
2020-07-24 19:57:17 -0300

    📱 Página Inicial (UI Finalizada & Validada com Formik)

--- react/src/routes.js
+++ react/src/routes.js
@@ -1,24 +1,29 @@
-import 'react-native-gesture-handler';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import "react-native-gesture-handler";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-import Temp from './pages/Temp';
-import Login from './pages/Login';
-import Cadastro from './pages/Cadastro'; 
-import VisualizacaoGeral from './pages/VisualizacaoGeral'; 
-import FormularioMetas from './pages/FormularioMeta.js';
+import Temp from "./pages/Temp";
+import PaginaInicial from "./pages/PaginaInicial";
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
+import VisualizacaoGeral from "./pages/VisualizacaoGeral";
+import FormularioMetas from "./pages/FormularioMeta.js";
 
 const Routes = createAppContainer(
-    createStackNavigator({
-        Temp,
-        Login,
-        Cadastro,
-        VisualizacaoGeral,
-        FormularioMetas,
-    }, {
-        headerMode: 'none',
-        initialRouteName: "Temp"
-    })
+	createStackNavigator(
+		{
+			Temp,
+			PaginaInicial,
+			Login,
+			Cadastro,
+			VisualizacaoGeral,
+			FormularioMetas,
+		},
+		{
+			headerMode: "none",
+			initialRouteName: "Temp",
+		}
+	)
 );
 
 export default Routes;
