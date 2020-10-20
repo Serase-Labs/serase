feefa272c9d2ab686272c0c426be05bbb8b36f35
Júlia <juliagonnzaga@gmail.com>
2020-10-20 15:55:08 -0300

    😊 Interface de Receitas/Despesa
    Pendência: alinhamento correto dos inputs secundários com o principal e adequar o espaço entre os botões.

--- react/src/pages/MovimentacaoComum.js
+++ react/src/pages/MovimentacaoComum.js
@@ -19,6 +19,7 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 // Componentes de navegação
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import { date } from "yup";
 // Documentação do Material Top (https://reactnavigation.org/docs/material-top-tab-navigator)
 const Tab = createMaterialTopTabNavigator();
 
@@ -38,7 +39,59 @@ function HomeScreen() {
 		<View
 			style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
 		>
-			<Text>Home!</Text>
+			<Text style={[estilos.labelInputPrincipal]}>
+				Quanto você ganhou?
+			</Text>
+			<TextInput 
+				style={[estilos.inputPrincipal]}
+				placeholder={"R$ 0,00"}
+				keyboardType={"numeric"}
+			>
+			</TextInput>
+
+			<Text style={[estilos.labelInput]}>
+				Data da Receita
+			</Text>
+			<TextInput 
+				style={estilos.input}
+				placeholder={"20/10/2020"}
+				placeholderTextColor={"#A0AEC0"}
+				keyboardType={"numeric"}
+			>
+			</TextInput>
+
+			<Text style={[estilos.labelInputcategoria]}>
+				Categoria de Receita
+			</Text>
+			<TextInput 
+				style={[estilos.input]}
+				placeholder={"Salário, Empréstimo, Loteria, Aposta"}
+				placeholderTextColor={"#A0AEC0"}
+			>
+			</TextInput>
+					<View 
+						style={{ flex: 1, flexDirection: "row" }}
+					>
+						<TouchableOpacity
+							style={estilos.botaoCancelar}
+							//onPress={handleSubmit}
+							title= "Submit"
+						>
+							<Text style={estilos.textoBotaoCancelar}>
+							Cancelar
+							</Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity
+							style={estilos.botaoAdicionar}
+							//onPress={handleSubmit}
+							title= "Submit"
+						>
+							<Text style={estilos.textoBotaoAdicionar}>
+							Adicionar
+							</Text>
+						</TouchableOpacity>
+					</View>
 		</View>
 	);
 }
@@ -48,7 +101,66 @@ function SettingsScreen() {
 		<View
 			style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
 		>
-			<Text>Settings!</Text>
+		<Text style={[estilos.labelInputPrincipal]}>
+				Quanto você gastou?
+			</Text>
+			<TextInput 
+				style={[estilos.inputPrincipalVermelho]}
+				placeholder={"R$ 0,00"}
+				keyboardType={"numeric"}
+			>
+			</TextInput>
+
+			<Text style={[estilos.labelInput]}>
+				Data de Gasto
+			</Text>
+			<TextInput 
+				style={estilos.input}
+				placeholder={"20/10/2020"}
+				placeholderTextColor={"#A0AEC0"}
+				keyboardType={"numeric"}
+			>
+			</TextInput>
+
+			<Text style={[estilos.labelInputcategoria]}>
+				Categoria do Gasto
+			</Text>
+			<TextInput 
+				style={[estilos.input]}
+				placeholder={"Alimentação, Trasnporte, Saúde"}
+				placeholderTextColor={"#A0AEC0"}
+			>
+			</TextInput>
+			<TextInput 
+				style={[estilos.input]}
+				placeholder={"Aplicativo de comida, trasnporte"}
+				placeholderTextColor={"#A0AEC0"}
+			>
+			</TextInput>
+
+					<View 
+						style={{ flex: 1, flexDirection: "row" }}
+					>
+						<TouchableOpacity
+							style={estilos.botaoCancelarVermelho}
+							//onPress={handleSubmit}
+							title= "Submit"
+						>
+							<Text style={estilos.textoBotaoCancelarVermelho}>
+							Cancelar
+							</Text>
+						</TouchableOpacity>
+
+						<TouchableOpacity
+							style={estilos.botaoAdicionarVermelho}
+							//onPress={handleSubmit}
+							title= "Submit"
+						>
+							<Text style={estilos.textoBotaoAdicionarVermelho}>
+							Adicionar
+							</Text>
+						</TouchableOpacity>	
+						</View>
 		</View>
 	);
 }
@@ -132,4 +244,27 @@ export default function MovimentacaoComum() {
 			</View> */}
 		</KeyboardAvoidingView>
 	);
+}
+
+const estilos = {
+	labelInputPrincipal: tailwind("text-gray-700 text-xl font-bold pb-8 mt-16"),
+	inputPrincipal: tailwind("border border-green-400 rounded font-bold text-4xl py-6 px-20 mb-16 text-gray-700 text base max-w-xs"),
+	inputPrincipalVermelho: tailwind("border border-red-700 rounded font-bold text-4xl py-6 px-20 mb-8 text-gray-700 text base max-w-xs"),
+
+	labelInput: tailwind("text-gray-700 text-base font-bold mb-3 mr-40"),
+	input: tailwind("border border-gray-700 rounded w-64 py-2 px-2 mx-12 mb-4 ml-8 text-gray-700 text base"),
+
+	labelInputcategoria: tailwind("text-gray-700 text-base font-bold mb-3 mr-40 ml-8"),
+
+	botaoAdicionar: tailwind("bg-green-400 py-2 rounded w-32 mb-8 mr-1 mt-12 ml-16"),
+	textoBotaoAdicionar: tailwind("text-white font-medium text-lg text-center"),
+	botaoCancelar: tailwind("py-2 rounded ml-8 mb-20 mt-12 mr-0"),
+	textoBotaoCancelar: tailwind("text-blue-700 font-medium text-lg text-center"),
+
+	botaoAdicionarVermelho: tailwind("bg-green-400 py-2 rounded w-32 mb-8 mr-1 mt-4 ml-16"),
+	textoBotaoAdicionarVermelho: tailwind("text-white font-medium text-lg text-center"),
+	botaoCancelarVermelho: tailwind("py-2 rounded ml-8 mb-20 mt-4 mr-0"),
+	textoBotaoCancelarVermelho: tailwind("text-blue-700 font-medium text-lg text-center"),
+	
+
 }
