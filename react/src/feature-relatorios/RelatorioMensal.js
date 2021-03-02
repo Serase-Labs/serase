6d351d62f3d76b2a401c88e67ee8b650dd0fb46a
Isabella Campos <isabellaemiliano14@gmail.com>
2021-03-02 00:01:42 -0300

    Relatório Mensal: Falta apenas um gráfico e o resolver o ngc das categorias iguais

--- react/src/feature-relatorios/RelatorioMensal.js
+++ react/src/feature-relatorios/RelatorioMensal.js
@@ -27,22 +27,30 @@ import {useState, useEffect} from "react";
 //////// Dados falsos utilizados para testes de gráficos
 //////// Serão substituídos por chamadas para os arquivos json
 //////// e, posteriormente, para chamadas do servidor+bd
-const DATA = [
-	{ titulo: "Maior Despesa", valor: "Lazer" },
-	{ titulo: "Maior Salto", valor: "Alimentação" },
-	{ titulo: "Maior Economia", valor: "Saúde" },
-];
+const DICIONARIO = {
+	"maior_despesa": "Maior Despesa",
+    "maior_salto": "Maior Salto",
+    "maior_economia": "Maior Economia"
+}
 
 export default function RelatorioMensal() {
-	const { user, token } = useAuth();
+	const { token } = useAuth();
 
-	const [categoria, setCategoria] = useState();
-	const [isLoadingCategoria, setLoadingCategoria] = useState(true);
+	const [categoria, setCategoria] = useState({}); 
 	const [isLoadingRelatorio, setLoadingRelatorio] = useState(true);
+
+	const converteAnalise = analises =>{
+		return Object.entries(analises).map(([titulo, valor])=>{
+			return {
+				titulo: (titulo in DICIONARIO)? DICIONARIO[titulo] : titulo, 
+				valor
+			}
+		});
+	}
 
 	useEffect(() => {
 		async function fetchRelatorioMensal() {
-			let url = GLOBAL.BASE_URL + "/relatorio/mensal/";
+			let url = GLOBAL.BASE_URL + "/relatorio/mensal";
 			console.log(url);
 			try {
 				let res = await fetch(url, {
@@ -51,7 +59,7 @@ export default function RelatorioMensal() {
 					},
 				});
 				let json = await res.json();
-				setCategoria(json);
+				setCategoria(json.conteudo);
 				setLoadingRelatorio(false);
 				console.log("BATATA");
 			} catch (error) {
@@ -59,165 +67,151 @@ export default function RelatorioMensal() {
 			}
 		}
 		fetchRelatorioMensal();
-
-		async function fetchCategoria() {
-			let url = GLOBAL.BASE_URL + "/analise/categoria/mensal/";
-			console.log(url);
-			try {
-				let res = await fetch(url, {
-					headers: {
-						Authorization: token,
-					},
-				});
-				let json = await res.json();
-				setCategoria(json);
-				setLoadingCategoria(false);
-				console.log("IRIRIRIRI");
-			} catch (error) {
-				console.log(error);
-			}
-		}
-		fetchCategoria();
 		
 	}, []);
 
 
-	const renderizarAnalises = ({categoria}) => {
+	const renderizarAnalises = ({item}) => {
 		return (
 			<BotaoInformacao
-				titulo={categoria}
-				valor={categoria}
-				onPress={() => console.log(categoria)}
+				titulo={item.titulo}
+				valor={item.valor}
+				id = {item.id}
+				onPress={() => console.log(item.titulo)}
 			/>
 		);
 	};
 
-	return (
-		<View style={tailwind("bg-white flex-1")}>
-			{/* Blocos de informações estáticos */}
-			<View style={tailwind("flex flex-row justify-between mx-5 mb-5")}>
-			{isLoadingCategoria ? (
-					<View style={tailwind("opacity-25")}>
-						<Text>Loading...</Text>
-					</View>
-				) : (
-					<View style={tailwind("py-4")}>
-						{renderizarAnalises(categoria)}
-					</View>
-				)}
+	if(isLoadingRelatorio){
+		// Caso o relatório não tenha carregado
+		return (
+			<View style={tailwind("opacity-25")}>
+				<Text>Loading...</Text>
 			</View>
+		);
+	} else
 
-			{/* Blocos de informações clicáveis */}
+		return (
 
-			<View style={tailwind("border-b border-t border-gray-100 py-4")}>
-				<View style={tailwind("mx-6 mb-4")}>
-					<Text style={tailwind("text-lg font-bold text-gray-800")}>
-						Análises
-					</Text>
-					<Text style={tailwind("text-base text-gray-800")}>
-						Em relação a última semana.
-					</Text>
+			<View style={tailwind("bg-white flex-1")}>
+				{/* Blocos de informações estáticos */}
+				<View style={tailwind("flex flex-row justify-between mx-5 mb-5")}>
+				<BlocoInformacao titulo="Gasto Total" conteudo={"R$"+(categoria.resumo.gasto_total*-1)} />
+				<BlocoInformacao titulo="Receita Total" conteudo={"R$"+(categoria.resumo.receita_total)} />
+				<BlocoInformacao titulo="Fluxo Total" conteudo={(categoria.resumo.fluxo_total>0? "+":"-")+"R$"+(categoria.resumo.fluxo_total)} />
 				</View>
 
-				<FlatList
+				{/* Blocos de informações clicáveis */}
+
+				<View style={tailwind("border-b border-t border-gray-100 py-4")}>
+					<View style={tailwind("mx-6 mb-4")}>
+						<Text style={tailwind("text-lg font-bold text-gray-800")}>
+							Análises
+						</Text>
+						<Text style={tailwind("text-base text-gray-800")}>
+							Em relação ao último mês.
+						</Text>
+					</View>
+
+					<FlatList
 					style={tailwind("ml-4")}
-					data = {categoria}
+					data={converteAnalise(categoria.analises)}
 					renderItem={renderizarAnalises}
 					horizontal={true}
-					keyExtractor={(categoria) => categoria.titulo}
+					keyExtractor={(item) => item.id}
 				/>
-			</View>
+				</View>
 
-			{/* Gráfico de despesa semanal */}
-			<View
-				style={tailwind("border-b border-t border-gray-100 py-4 px-6")}
-			>
-				<Text style={tailwind("mb-5 text-lg font-bold text-gray-800")}>
-					Gráficos
-				</Text>
-
-				<TabDescricao
-					conteudo="Despesas por categoria"
-					descricao="Suas áreas de maior gasto este mês."
+				{/* Gráfico de despesa semanal */}
+				<View
+					style={tailwind("border-b border-t border-gray-100 py-4 px-6")}
 				>
-					<GraficoDespesaCategoria />
-				</TabDescricao>
+					<Text style={tailwind("mb-5 text-lg font-bold text-gray-800")}>
+						Gráficos
+					</Text>
 
-				<TabDescricao
-					conteudo="Despesas por padrão"
-					descricao="Seus gastos em despesas fixas e variáveis."
-				>
-					<GraficoDespesaPadrao />
-				</TabDescricao>
-				<TabDescricao
-					conteudo="Frequência de despesas"
-					descricao="Distribuição dos seus gastos pelo mês."
-				>
-					<GraficoDespesaFrequencia />
-				</TabDescricao>
-			</View>
+					<TabDescricao
+						conteudo="Despesas por categoria"
+						descricao="Suas áreas de maior gasto este mês."
+					>
+						<GraficoDespesaCategoria periodo="mensal"/>
+					</TabDescricao>
 
-			{/* Seção de redirecionamento1*/}
-			<View
-				style={tailwind(
-					"mt-4 mb-12 mx-5 py-4 bg-gray-100 flex items-center rounded-md"
-				)}
-			>
-				<Text
+					<TabDescricao
+						conteudo="Despesas por padrão"
+						descricao="Seus gastos em despesas fixas e variáveis."
+					>
+						<GraficoDespesaPadrao periodo="mensal"/>
+					</TabDescricao>
+					<TabDescricao
+						conteudo="Frequência de despesas"
+						descricao="Distribuição dos seus gastos pelo mês."
+					>
+						<GraficoDespesaFrequencia />
+					</TabDescricao>
+				</View>
+
+				{/* Seção de redirecionamento1*/}
+				<View
 					style={tailwind(
-						"my-4 text-base text-gray-500 w-64 text-center"
+						"mt-4 mb-12 mx-5 py-4 bg-gray-100 flex items-center rounded-md"
 					)}
 				>
-					Veja todas as suas despesas da semana em detalhes.
-				</Text>
+					<Text
+						style={tailwind(
+							"my-4 text-base text-gray-500 w-64 text-center"
+						)}
+					>
+						Veja todas as suas despesas do mês em detalhes.
+					</Text>
 
-				<View
-					style={tailwind("flex flex-row w-56 justify-between mb-3")}
-				>
-					<TouchableOpacity
-						style={[
-							tailwind(
-								"w-24 h-24 bg-gray-200 flex justify-center items-center rounded-md"
-							),
-							{ elevation: 2 },
-						]}
-						onPress={() => navigation.navigate("Receitas")}
+					<View
+						style={tailwind("flex flex-row w-56 justify-between mb-3")}
 					>
-						<View style={tailwind("w-8 h-8 mb-1")}>
-							<IconeReceita uso="sistema" />
-						</View>
-						<Text
+						<TouchableOpacity
 							style={[
-								tailwind("text-base text-blue-900"),
-								{ fontWeight: "bold" },
+								tailwind(
+									"w-24 h-24 bg-gray-200 flex justify-center items-center rounded-md"
+								),
+								{ elevation: 2 },
 							]}
+							onPress={() => navigation.navigate("Receitas")}
 						>
-							Receitas
-						</Text>
-					</TouchableOpacity>
-					<TouchableOpacity
-						style={[
-							tailwind(
-								"w-24 h-24 bg-gray-200 flex justify-center items-center rounded-md"
-							),
-							{ elevation: 1 },
-						]}
-						onPress={() => navigation.navigate("Despesas")}
-					>
-						<View style={tailwind("w-8 h-8 mb-1")}>
-							<IconeDespesa uso="sistema" />
-						</View>
-						<Text
+							<View style={tailwind("w-8 h-8 mb-1")}>
+								<IconeReceita uso="sistema" />
+							</View>
+							<Text
+								style={[
+									tailwind("text-base text-blue-900"),
+									{ fontWeight: "bold" },
+								]}
+							>
+								Receitas
+							</Text>
+						</TouchableOpacity>
+						<TouchableOpacity
 							style={[
-								tailwind("text-base text-blue-900"),
-								{ fontWeight: "bold" },
+								tailwind(
+									"w-24 h-24 bg-gray-200 flex justify-center items-center rounded-md"
+								),
+								{ elevation: 1 },
 							]}
+							onPress={() => navigation.navigate("Despesas")}
 						>
-							Despesas
-						</Text>
-					</TouchableOpacity>
+							<View style={tailwind("w-8 h-8 mb-1")}>
+								<IconeDespesa uso="sistema" />
+							</View>
+							<Text
+								style={[
+									tailwind("text-base text-blue-900"),
+									{ fontWeight: "bold" },
+								]}
+							>
+								Despesas
+							</Text>
+						</TouchableOpacity>
+					</View>
 				</View>
 			</View>
-		</View>
-	);
+		);
 }
